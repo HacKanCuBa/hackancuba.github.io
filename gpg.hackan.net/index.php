dff9d855e8b078c13731caf914ca532c331967be
HacKan <hackan@gmail.com>
2016-06-09 19:55:13 -0300

    added subdomain for personal gpg key

---
+++ gpg.hackan.net/index.php
@@ -0,0 +1,25 @@
+<?php
+cont KEYFILE = '0xECF0573B1C9B59E8.gpg';
+
+$download = isset($_GET['d']) ?: isset($_GET['f']);
+$response = 'Unknow error';
+
+if (file_exists(KEYFILE)) {
+	header('Content-Description: HacKan\'s GPG key');
+	if ($download) {
+		header('Content-Type: application/octet-stream');
+		header('Content-Disposition: attachment; filename="' . basename(KEYFILE) . '"');
+	} else {
+		header('Content-Type: text/plain; charset=utf-8');
+		header('Content-Disposition: inline');
+	}
+	header('Expires: 0');
+	header('Cache-Control: must-revalidate');
+	header('Pragma: public');
+	header('Content-Length: ' . filesize(KEYFILE));
+	$response = readfile(KEYFILE);
+} else {
+	$response = "File: " . KEYFILE . " doesn't exists!";
+}
+
+exit($response);
